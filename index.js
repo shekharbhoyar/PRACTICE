b3b56d5959637ab5afa9f6809a507664d3f8e5d6
chandrashekhar <shekharbhoyar10@gmail.com>
2024-12-17 00:32:57 +0530

    palendrome question

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 import hofarray from "./Array methods/reverse.js";
 import loopfuction from "./Array methods/reverse.js";
 import results from "./Array methods/reverse.js";
+import data from "./data.js";
 // loopfuction();
 // console.log(results);
-console.log(hofarray);
+// console.log(hofarray);
+
+// for (const d of data.reverse()) {
+//   console.log(`${d.date}| ${d.maxTemp}`);
+// }
+
+const isPalendrome = (word) => {
+  let array = [];
+  let revArray = [];
+  for (const w of word) {
+    array.push(w);
+    revArray.push(w);
+  }
+  return array.toString() === revArray.reverse().toString();
+};
+
+console.log(isPalendrome("civic") ? "Is a palendrom" : "not a palendrome");
