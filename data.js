2b93b907736cc23d865d9e38824af0019431d000
chandrashekhar <shekharbhoyar10@gmail.com>
2024-12-16 23:10:13 +0530

    date and temp  rel data added

---
+++ data.js
@@ -0,0 +1,8 @@
+const data = [
+  { id: 1, date: "22-dec-2024", maxTemp: 31, minTemp: 19 },
+  { id: 2, date: "21-dec-2024", maxTemp: 34, minTemp: 20 },
+  { id: 3, date: "23-dec-2024", maxTemp: 32, minTemp: 18 },
+  { id: 4, date: "24-dec-2024", maxTemp: 31, minTemp: 19 },
+  { id: 5, date: "25-dec-2024", maxTemp: 30, minTemp: 21 },
+];
+export default data;
